399657254fceeea8f2ec2d257b076e7ab92b9693
Thomas Dem <demagny.t@hotmail.fr>
2019-07-03 17:12:44 +0200

    update index.php

--- public/index.php
+++ public/index.php
@@ -1,23 +1,16 @@
 <?php
 
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 use Controller\FrontController;
 
 use Tracy\Debugger;
 
-<<<<<<< Updated upstream
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
+
 Debugger::enable();
 
-$frontController
-=======
-// Loads Composer autoload
-require_once '../vendor/autoload.php';
-Debugger::enable();
 $frontController = new FrontController();
 $frontController->run();
->>>>>>> Stashed changes
+
