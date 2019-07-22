d694e77836388609884c78626679372bc76d055a
Thomas Dem <demagny.t@hotmail.fr>
2019-07-22 19:24:20 +0200

    add new user connexion
    UserController UserModel and tied up connexion view

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,11 +4,44 @@
 namespace Controller;
 
 
+use Model\UserModel;
+
 /**
  * Class UserController
  * @package Controller
  */
-class UserController
+class UserController extends Controller
 {
+    /**
+     * @return \Twig\Environment
+     */
+    public function indexAction()
+    {
 
+        return $this->render('connexion.twig');
+    }
+
+    /**
+     * @return \Twig\Environment
+     */
+    public function signupAction()
+    {
+
+        $info['firstname'] = filter_input(INPUT_POST, 'signupfirstname', FILTER_SANITIZE_SPECIAL_CHARS);
+        $info['surname'] = filter_input(INPUT_POST, 'signupsurname', FILTER_SANITIZE_SPECIAL_CHARS);
+        $info['username'] = filter_input(INPUT_POST, 'signupusername', FILTER_SANITIZE_SPECIAL_CHARS);
+        $info['email'] = filter_input(INPUT_POST, 'signupemail', FILTER_SANITIZE_SPECIAL_CHARS);
+        $info['password'] = filter_input(INPUT_POST, 'signuppassword', FILTER_SANITIZE_STRING);
+        $info['confirmpassword'] = filter_input(INPUT_POST, 'confirmpassword', FILTER_SANITIZE_STRING);
+        $usermodel = new UserModel();
+
+        $data = $usermodel->controlUser($info ['username'], $info['email']);
+
+        if ($data == true) {
+            echo "<script>alert('erreur ! Un pseudo ou une adresse mail est déjà enregistrée, veuillez vous connecter .');</script>";
+            return $this->render('connexion.twig');
+        }
+    }
 }
+
+
--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -10,14 +10,32 @@ namespace Model;
  */
 class UserModel extends PDOConnexion
 {
-    public function controlUser($nickname, $email)
+    /**
+     * @param $username
+     * @param $email
+     * @return bool
+     */
+    public function controlUser($username, $email)
     {
         $database = $this->databaseConnexion();
-        $req = $database->prepare('SELECT * FROM user WHERE nickname = ? OR email = ?  LIMIT 1');
-        $req->execute(array($nickname, $email));
-
-        return true;
+        $req = $database->prepare('SELECT * FROM user WHERE username = ? OR email = ?  LIMIT 1');
+        $req->execute(array($username, $email));
+        if ($req->fetch()) {
+            return true;
+        }
+        return false;
     }
 
-//return quoi? requete?
+
+    /**
+     * @param $info
+     */
+    public function addUser($info){
+
+        $database = $this->databaseConnexion();
+        $req = $database->prepare("INSERT INTO user (firstname, surname, username, email, password) VALUES (?,?,?,?,?)");
+        $req->execute(array($info['firstname'], $info['surname'], $info['username'], $info['email'], $info['password']));
+    }
+
+
 }
