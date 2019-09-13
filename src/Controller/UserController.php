6727229ac7e316f072bd8316072df819967f8464
Thomas Dem <demagny.t@hotmail.fr>
2019-09-13 16:25:52 +0200

    add logAction and rename signup to register

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -16,12 +16,11 @@ use Twig\Error\SyntaxError;
  */
 class UserController extends Controller
 {
-
     /**
      * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function indexAction()
     {
@@ -31,11 +30,11 @@ class UserController extends Controller
 
     /**
      * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
-    public function signupAction()
+    public function registerAction()
     {
         $info['username'] = filter_input(INPUT_POST, 'signup_username', FILTER_SANITIZE_SPECIAL_CHARS);
         $info['password'] = filter_input(INPUT_POST, 'signup_password', FILTER_SANITIZE_STRING);
@@ -45,13 +44,13 @@ class UserController extends Controller
         $info['email'] = filter_input(INPUT_POST, 'signup_email', FILTER_SANITIZE_SPECIAL_CHARS);
 
 
-        $userModel = new UserManager();
+        $userManager = new UserManager();
 
 
-        if (empty($info['username']) || empty($info['email']) || $userModel->controlUser($info)) {
-            echo "<script>alert('Un pseudo et/ou une adresse mail existe déjà, merci de vous connecter')</script>";
+        if ($userManager->controlUser($info['username'], $info['email'])) {
+            $this->alert("Un pseudo et/ou une adresse mail existe déjà, merci de vous connecter");
 
-            return $this->render('connexion.twig', array('info' => $info));
+            return $this->render('connexion.twig', compact('info'));
 
 
         }
@@ -59,40 +58,65 @@ class UserController extends Controller
         if (!empty($info['firstname']) AND !empty($info['surname']) AND !empty($info['password']) AND !empty($info['confirmpassword'])) {
             if ($info['password'] == $info['confirmpassword']) {
                 $info['password'] = password_hash($info['password'], PASSWORD_BCRYPT);
-                $userModel->addUser($info);
-                echo "<script>alert('Merci vous êtes bien inscrit')</script>";
+                $userManager->insertUser($info);
+                $this->alert("Merci vous êtes bien inscrit");
                 return $this->render('home.twig');
             }
-            echo "<script>alert('Vos deux mots de passe doivent être identique')</script>";
-            return $this->render('connexion.twig', array('info' => $info));
+            $this->alert("Vos deux mots de passe doivent être identique");
+            return $this->render('connexion.twig', compact('info'));
 
         }
         return $this->render('connexion.twig');
 
     }
 
-    /**
-     *
-     * @return string
-     */
-    public function signinAction()
-    {
-        $info['username'] = filter_input(INPUT_POST, 'signup_username', FILTER_SANITIZE_SPECIAL_CHARS);
-        $info['password'] = filter_input(INPUT_POST, 'signup_password', FILTER_SANITIZE_STRING);
 
-        if (!empty($info['username']) && !empty($info['password'])) {
-            $userModel = new UserManager();
-            $user = $userModel->controlUser($info['username']);
-            if ($user !== false) {
-                $user = $userModel->getUser($info['username'], $info['password']);
-                if (password_verify($info['password'], $info['confirmpassword']) == true) ;
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function logAction()
+    {
+        $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+
+//on vérifie que username et password ne soient pas vide
+        if (!empty($username) and !empty($password)) {
+            $userManager = new UserManager();
+            $this->alert("ca bloque !");
+
+//Vérifie que l’utilisateur existe avec le username
+            $user = $userManager->verifyUser($username);
+                //Ensuite on récupère ses données
+                if ($user !== false) {
+                    $user = $userManager->find($username);
+                    //Et on vérifie le password
+                    $this->alert("ca bloque pas !");
+                    if (password_verify($password, $user['password']) === true) {
+                        $this->session->create($user['id'], $user['username'], $user['email']);
+
+                        $this->alert("Bienvenue chez Tom's Blog !");
+                        return $this->render('home.twig', array('session' => filter_var_array($_SESSION)));
+
+                    }
+                }
             }
+
+
+            $this->alert('Pseudo ou mot de passe incorrect !');
+            return $this->render('portfolio.twig');
         }
 
 
-        return;
-    }
 }
+
+
+
+
+
+
 
 
 
