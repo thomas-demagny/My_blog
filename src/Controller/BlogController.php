89366aa2a577eaf9f91a41af8eb3797dba63b8de
Thomas Dem <demagny.t@hotmail.fr>
2019-09-13 16:24:12 +0200

    add commentAction

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,45 +3,83 @@
 
 namespace Controller;
 
-use Model\articleManager;
-use Model\commentManager;
 
+use Model\ArticleManager;
+use Model\CommentManager;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 /**
  * Class BlogController
  * @package Controller
  */
 class BlogController extends Controller
+
 {
-
-
     /**
      * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
-    public function indexAction(){
-
-$articles = (new articleManager)->getArticles();
-        return $this->render('blog.twig',array('articles' =>$articles));
+    public function indexAction()
+    {
+        $articles = (new ArticleManager)->findAll();
+        return $this->render('blog.twig', compact('articles'));
     }
 
 
     /**
      * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
-    public function readAction(){
+    public function commentAction()
+    {
+        $articles_id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+        $content = filter_input(INPUT_POST, 'comments', FILTER_SANITIZE_STRING);
 
+        if (!empty($articles_id) && !empty($content)) {
+
+            $uid = $this->session->getUser('id');
+            $author = $this->session->getUser('username');
+            $commentManager = new CommentManager();
+            $commentManager->insert($author, $content, $articles_id, $uid);
+            //on recupère l'article et le commentaire
+            $comments = $commentManager->findAll($articles_id);
+            $ArticleManager = new ArticleManager();
+            $articles = $ArticleManager->find($articles_id);
+            return $this->render('article.twig', compact('author', 'content', 'articles_id', 'uid', 'articles', 'comments'));
+        }
+        return $this->render('article.twig', compact('author', 'content', 'articles_id', 'uid'));
+
+
+        }
+
+
+
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function readAction()
+    {
         $dataId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-        $articles = (new articleManager)->getArticle($dataId);
-        $comments = (new commentManager)->getComments($dataId);
+        $articles = (new ArticleManager)->find($dataId);
+        $comments = (new CommentManager)->findAll($dataId);
 
-        return $this->render('article.twig', array('articles' => $articles, 'comments' => $comments));
+        return $this->render('article.twig', compact('articles', 'comments'));
     }
 
 }
+
+
+
+
+
 
