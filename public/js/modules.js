e0b65cb8432f262848f7aaab900f2cba5b16f768
Thomas Dem <demagny.t@hotmail.fr>
2019-09-24 19:30:20 +0200

    renamed files

--- public/js/modules.js
+++ public/js/modules.js
@@ -1,4 +1,4 @@
-function verify() {
+function modules() {
     var signup_password = document.getElementById("signup_password").value;
     var confirmpassword = document.getElementById("confirmpassword").value;
     if (signup_password !== confirmpassword) {
@@ -8,5 +8,15 @@ function verify() {
         document.getElementById("msg").innerHTML = "Valide";
         document.getElementById("msg").style.color = "green";
     }
-
 }
+
+
+//switch tabs in admin
+    $('#adminTab a').on('click', function (admin) {
+        admin.preventDefault();
+        $(this).tab('show')
+    });
+
+
+
+
