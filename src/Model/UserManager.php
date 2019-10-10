50a939d90bf10630fff57079d5b0e0af11456a2e
Thomas Dem <demagny.t@hotmail.fr>
2019-10-10 10:49:03 +0200

    added delete user

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -4,22 +4,59 @@
 namespace Model;
 
 
-use Model;
+
 
 /**
  * Class UserManager
  * @package Model
  */
-class UserManager extends Model\Model
+class UserManager extends Model
 {
+
+//protected $table = 'user';
+
     /**
      * @param $username
+     * @return mixed
+     */
+    public function find($username)
+    {
+
+        $result = $this->pdo->prepare('SELECT * FROM user WHERE username = ?');
+        $result->execute (array($username));
+        return $result->fetch();
+    }
+
+    /**
+     * @return array
+     */
+    public function findAll()
+    {
+        $result = $this->pdo->prepare("SELECT * FROM user ORDER BY username");
+        $result->execute();
+        return $result->fetchAll();
+    }
+    /**
+     * @param $info
+     * @return bool
+     */
+    public function insertUser($info)
+    {
+
+
+        $result = $this->pdo->prepare("INSERT INTO user (firstname, surname, username, email, password, role, register_date) VALUES (?,?,?,?,?,'membre', CURRENT_DATE)");
+        $result = $result->execute(array(
+            $info['firstname'], $info['surname'], $info['username'], $info['email'], $info['password']));
+        return $result;
+    }
+    /**
+     * @param $username
+     * @param $email
      * @return bool
      */
     public function controlUser($username, $email)
     {
-        $pdo = $this->getPDO();
-        $result = $pdo->prepare('SELECT * FROM user WHERE username = ?  OR email = ? LIMIT 1');
+        $result = $this->pdo->prepare('SELECT * FROM user WHERE username = ?  OR email = ? LIMIT 1');
         $result->execute(array($username, $email));
         if ($result->fetch()) {
             return true;
@@ -33,8 +70,7 @@ class UserManager extends Model\Model
      */
     public function verifyUser($username)
     {
-        $pdo = $this->getPDO();
-        $result = $pdo->prepare('SELECT * FROM user WHERE username= ?');
+        $result = $this->pdo->prepare('SELECT * FROM user WHERE username= ?');
         $result->execute(array($username));
         if ($result->fetch()) {
             return true;
@@ -42,44 +78,15 @@ class UserManager extends Model\Model
         return false;
     }
 
-    /**
-     * @param $username
-     * @return mixed
-     */
-    public function find($username)
-    {
-        $pdo = $this->getPDO();
-        $result = $pdo->prepare('SELECT * FROM user WHERE username = ?');
-        $result->execute (array($username));
-        return $result->fetch();
-    }
-    /**
-     * @return array
-     */
-    public function findAll()
-    {
-        $pdo = $this->getPDO();
-        $result = $pdo->prepare('SELECT * FROM user ORDER BY register_date AND role');
-        $result->execute();
-        return $result->fetchAll();
-    }
 
     /**
-     * @param $info
-     * @return bool
+     * @param string $username
      */
-    public function insertUser($info)
+    public function delete(string $username)
     {
-
-        $pdo = $this->getPDO();
-        $result = $pdo->prepare("INSERT INTO user (firstname, surname, username, email, password, role, register_date) VALUES (?,?,?,?,?,'membre', CURRENT_DATE)");
-        $result = $result->execute(array(
-            $info['firstname'], $info['surname'], $info['username'], $info['email'], $info['password']));
-        return $result;
+        $req = $this->pdo->prepare('DELETE FROM user WHERE username= ? ');
+        $req->execute(array($username));
     }
-
-
-
 
 
 }
