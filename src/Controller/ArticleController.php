49d6e3a595f50de9b4d992852c433e8f0e84db8a
Thomas Dem <demagny.t@hotmail.fr>
2019-10-22 11:54:27 +0200

    methods create, delete, update created

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,8 +10,15 @@ use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
 
+
+/**
+ * Class ArticleController
+ * @package Controller
+ */
 class ArticleController extends Controller
 {
+
+
     /**
      * @return string
      * @throws LoaderError
@@ -20,7 +27,9 @@ class ArticleController extends Controller
      */
     public function indexAction()
     {
-        $articles = (new ArticleManager)->findAll();
+        $articlesManager = new ArticleManager();
+        $articles = $articlesManager->findAll();
+
 
         return $this->render('blog.html.twig', compact('articles'));
     }
@@ -34,13 +43,63 @@ class ArticleController extends Controller
     public function readAction()
     {
         $dataId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-        $articles = (new ArticleManager)->find($dataId);
+        $articlesManager = new ArticleManager();
+        $articles = $articlesManager->find($dataId);;
 
-        $comments = (new CommentManager)->findAll($dataId);;
+        $commentManager = new CommentManager();
+        $comments = $commentManager->findAll($dataId);
 
 
         return $this->render('article.html.twig', compact('articles', 'comments'));
 
-
     }
+
+    /**
+     * @param array $info
+     */
+    public function createAction()
+    {
+        $info['title'] = filter_input(INPUT_POST, 'title');
+        $info['chapo'] = filter_input(INPUT_POST, 'chapo');
+        $info['content'] = filter_input(INPUT_POST, 'create');
+        $info['author'] = $this->session->getUser('username');
+
+
+
+        $articleManager = new ArticleManager();
+        $articleManager->insert($info);
+        $this->redirect('../public/index.php?access=admin');
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function updateAction()
+    {
+        $dataId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+        $articlesManager = new ArticleManager();
+        $articles = $articlesManager->find($dataId);
+        //va chercher l'article avec le manager get
+        //render vers ma vue modifiée en chargeant l'article
+        return $this->render('article.html.twig', compact('articles'));
+    }
+
+    /**
+     *on supprime les commentaires puis l'article
+     */
+    public function deleteAction()
+    {
+        $dataId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+        $commentManager = new CommentManager();
+        $commentManager->deleteFromArticle($dataId);
+        $articleManager = new ArticleManager();
+        $articleManager->delete($dataId);
+        $this->redirect('../public/index.php?access=admin');
+    }
+
+
 }
